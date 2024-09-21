ef994aed824af66bab539a6d29bc95aa84c7dc3c
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-21 17:33:56 +0300

    Add welcome message to BankAccount constructor.

--- bank_account_models.py
+++ bank_account_models.py
@@ -2,6 +2,9 @@ class BankAccount:
     def __init__(self, acc_name, initial_amount):
         self.name = acc_name
         self.balance = initial_amount
+        
+        # show a welcome message with details after each account creation
+        print(f"✅ Account successfully created!\nAccount Name: {self.name}\t\t\tAccount Balance: ${self.balance}\n")
 
 
 # Create an instance of the class
