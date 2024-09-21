84abd657aa2adbf9715b8216e8d2f8d227d4a8a0
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-21 22:21:07 +0300

    Fix bug: initial amount was passed as a string.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bank_account_models import *
 
 # Create an instance of the class
-khaled = BankAccount("Khaled", "500")
+khaled = BankAccount("Khaled", 500)
 
 # Get and print account name and balance
 print(khaled.name)  # OUTPUT: Khaled
 print(khaled.balance)  # OUTPUT: 500
+
